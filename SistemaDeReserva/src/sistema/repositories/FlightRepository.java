ab8591b31127543223e4c49a0f84103fce6e479b
Augusto Nunes Zacarias <augustonz2010@hotmail.com>
2023-08-20 17:37:48 -0300

    Pass search for flights test

--- SistemaDeReserva/src/sistema/repositories/FlightRepository.java
+++ SistemaDeReserva/src/sistema/repositories/FlightRepository.java
@@ -1,7 +1,10 @@
 package sistema.repositories;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import sistema.models.flight.*;
 
@@ -32,9 +35,95 @@ public class FlightRepository {
 		return flights.get(id);
 	}
 	
+	public void setAvailability(int id,boolean value) {
+		flights.get(id).setAvailable(value);
+	}
+	
+	Map<Integer,Flight> getSubMap(List<Integer> keyList) {
+		Map<Integer, Flight> newMap = new HashMap<>(flights);
+		newMap.keySet().retainAll(keyList);
+		return newMap;
+	}
+	
+	public Map<Integer,Flight> searchFlightsByOrigin(String origin) {
+		List<Integer> keys = new ArrayList<>();
+		
+		for (Entry<Integer, Flight> entry : flights.entrySet()) {
+	        if (entry.getValue().getOrigin().equals(origin)) {
+	            keys.add(entry.getKey());
+	        }
+	    }
+		
+		return getSubMap(keys);
+	}
+	
+	public Map<Integer,Flight> searchFlightsByDestination(String destination) {
+		List<Integer> keys = new ArrayList<>();
+		
+		for (Entry<Integer, Flight> entry : flights.entrySet()) {
+	        if (entry.getValue().getDestination().equals(destination)) {
+	            keys.add(entry.getKey());
+	        }
+	    }
+		
+		return getSubMap(keys);
+	}
+	
+	public Map<Integer,Flight> searchFlightsByPrice(double price) {
+		List<Integer> keys = new ArrayList<>();
+		
+		for (Entry<Integer, Flight> entry : flights.entrySet()) {
+	        if (entry.getValue().getPrice()<=price) {
+	            keys.add(entry.getKey());
+	        }
+	    }
+		
+		return getSubMap(keys);
+	}
+	
+	public Map<Integer,Flight> searchFlightsByDate(String date) {
+		List<Integer> keys = new ArrayList<>();
+		
+		for (Entry<Integer, Flight> entry : flights.entrySet()) {
+	        if (entry.getValue().getDate().equals(date)) {
+	            keys.add(entry.getKey());
+	        }
+	    }
+		
+		return getSubMap(keys);
+	}
+	
+	public Map<Integer,Flight> searchFlightsBySeatCount(int num) {
+		List<Integer> keys = new ArrayList<>();
+		
+		for (Entry<Integer, Flight> entry : flights.entrySet()) {
+	        if (entry.getValue().getAvailableSeats()>=num) {
+	            keys.add(entry.getKey());
+	        }
+	    }
+		
+		return getSubMap(keys);
+	}
+	
+	public Map<Integer,Flight> searchFlightsByAvailability() {
+		List<Integer> keys = new ArrayList<>();
+		
+		for (Entry<Integer, Flight> entry : flights.entrySet()) {
+	        if (entry.getValue().isAvailable()) {
+	            keys.add(entry.getKey());
+	        }
+	    }
+		
+		return getSubMap(keys);
+	}
+	
 	public String listAllFlights() {
+		return listFlights(flights);
+	}
+	
+	public String listFlights(Map<Integer,Flight> flights) {
 		String resp = "";
-		for (Map.Entry<Integer, Flight> entry : flights.entrySet()) {
+		for (Entry<Integer, Flight> entry : flights.entrySet()) {
 			resp += entry.getKey() + ": " + entry.getValue() + "\n";
 	    }
 		
