277e88adf0404ad07da0f93ee90ea36c174cfd71
Augusto Nunes Zacarias <augustonz2010@hotmail.com>
2023-08-20 14:51:08 -0300

    Add new basic user tests

--- SistemaDeReserva/tests/sistema/models/user/UserTest.java
+++ SistemaDeReserva/tests/sistema/models/user/UserTest.java
@@ -8,13 +8,20 @@ public class UserTest {
 	User testUser;
 	
 	@Before
-	public void CreateTestUser() {
+	public void createTestUser() {
 		testUser = new User();
 	}
 	
 	@Test
-	public void BasicUser() {	  
+	public void basicUser() {	  
 		assertEquals(testUser.getName(),"Default Name");
 		assertEquals(testUser.getCpf(),"000.000.000-00");
+	}
+	
+	@Test
+	public void specificUser() {
+		User newUser = new User("Fulano","111.222.333-44");
+		assertEquals(newUser.getName(),"Fulano");
+		assertEquals(newUser.getCpf(),"111.222.333-44");
 	}
 }
