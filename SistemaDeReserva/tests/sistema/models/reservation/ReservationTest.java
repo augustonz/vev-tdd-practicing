f27904a91f449912a2a02b510e1e914bd467529d
Augusto Nunes Zacarias <augustonz2010@hotmail.com>
2023-08-20 18:26:21 -0300

    Add print reservation tests

--- SistemaDeReserva/tests/sistema/models/reservation/ReservationTest.java
+++ SistemaDeReserva/tests/sistema/models/reservation/ReservationTest.java
@@ -15,16 +15,30 @@ public class ReservationTest {
 	Flight testFlight;
 	
 	@Before
-	public void createTestFlight() {
+	public void createTestFlightTest() {
 		testUser = new User();
 		testFlight = new Flight("João Pessoa","Recife","01/01/2023","13:00",true,35.00,48);
 		testReservation = new Reservation(testUser,4,testFlight); //usuario, numero de passagens, voo
 	}
 	
 	@Test
-	public void basicReservation() {	  
+	public void basicReservationTest() {	  
 		assertEquals(testReservation.getUser(),testUser);
 		assertEquals(testReservation.getTicketCount(),4);
 		assertEquals(testReservation.getFlight(),testFlight);
+	}
+	
+	@Test
+	public void toStringTest() {
+		System.out.println(testReservation);
+		assertEquals(testReservation.toString(),
+				"Informações do Voo:\n"
+				+ "Voo de João Pessoa para Recife, data: 01/01/2023 às 13:00.\n"
+				+ "Voo disponível? Sim, preço: R$35.0\n"
+				+ "Passageiros: 0/48\n"
+				+ "\n"
+				+ "Usuário emissor - Nome: Default Name, cpf: 000.000.000-00, telefone para contato: +55(83)91234-5678\n"
+				+ "\n"
+				+ "Preço total da reserva: R$140.0");
 	}
 }
--- SistemaDeReserva/tests/sistema/models/user/UserTest.java
+++ SistemaDeReserva/tests/sistema/models/user/UserTest.java
@@ -13,17 +13,22 @@ public class UserTest {
 	}
 	
 	@Test
-	public void basicUser() {	  
+	public void basicUserTest() {	  
 		assertEquals(testUser.getName(),"Default Name");
 		assertEquals(testUser.getCpf(),"000.000.000-00");
 		assertEquals(testUser.getContato(),"+55(83)91234-5678");
 	}
 	
 	@Test
-	public void specificUser() {
+	public void specificUserTest() {
 		User newUser = new User("Fulano","111.222.333-44","+55(83)90000-0000");
 		assertEquals(newUser.getName(),"Fulano");
 		assertEquals(newUser.getCpf(),"111.222.333-44");
 		assertEquals(newUser.getContato(),"+55(83)90000-0000");
+	}
+	
+	@Test
+	public void UserToStringTest() {
+		assertEquals(testUser.toString(),"Usuário emissor - Nome: Default Name, cpf: 000.000.000-00, telefone para contato: +55(83)91234-5678");
 	}
 }
